3adf0cc59800db7375cbf85727eb2f46fe1aef91
Hayin Ananta <72958200+hayinananta@users.noreply.github.com>
2020-12-04 21:33:04 +0700

    Add files via upload

---
+++ src/uts/if1/pkg10119005/hayinananta/no1/UTSIF110119005HAYINANANTANo1.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.if1.pkg10119005.hayinananta.no1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Hayin
+ * NAMA     : Hayin Ananta
+ * KELAS    : IF-1
+ * NIM      : 10119005
+ * Deskripsi Program : Program ini untuk menampilkan deskripsi seseorang seperti 
+ *                     zodiak dengan berbasis Objek
+ * 
+ */
+public class UTSIF110119005HAYINANANTANo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        Age umur = new Age(2020);
+        
+        System.out.print("Masukkan Tahun Lahir Anda : ");
+        umur.setYearBirth(scanner.nextInt());
+        
+        System.out.println();
+        System.out.println();
+        System.out.println("======Hasil Perhitungan Umur======");
+        int yearBirth = umur.getYearBirth();
+        System.out.println("Tahun Lahir Anda : " + yearBirth);
+        int yearNow = umur.getYearNow();
+        System.out.println("Hari ini Tahun : " + yearNow);
+        int usia = umur.hitungUmur();
+        System.out.println("Umur kamu sampai hari ini adalah " + usia + " tahun");
+        System.out.println("Tandanya Kamu " + umur.tandanyaKamu(usia));
+    }
+    
+}
